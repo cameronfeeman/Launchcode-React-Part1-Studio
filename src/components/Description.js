bdad58f986bb075578b832cd1f66d32887476b10
cameronfeeman <139170459+cameronfeeman@users.noreply.github.com>
2023-08-03 21:31:50 -0400

    new studio

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import styles from './Description.module.css'
 function RecipeAuthor() {
     let authorLink;
     let authorPhoto;
     let authorName;
-}
+
+    return (
+        <div className = {StyleSheet.recipeAuthorBlock}>
+            <img src={authorphoto} alt=""
+            className={StyleSheet.imageUpdates} />
+            <div>
+                <h3>{authorName}</h3>
+                <a href={authorLink}>Blog name </a>
+                </div>
+        </div>
+        )};
