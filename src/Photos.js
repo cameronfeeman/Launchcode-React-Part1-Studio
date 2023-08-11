e981d58b863e8aee0d6da761d1dea4fb11a8e8b8
cameronfeeman <139170459+cameronfeeman@users.noreply.github.com>
2023-08-11 16:57:31 -0400

    on last part add more CSS

---
+++ src/Photos.js
@@ -0,0 +1,9 @@
+import styles from './Description.module.css';
+
+const RecipePhoto = () => {
+   return <img src="https://www.kingarthurbaking.com/sites/default/files/styles/featured_image_2x/public/2020-12/perfectly-pillowy-cinnamon-rolls.jpg?itok=vbckO62q" 
+   alt="Cinnamon Rolls" 
+   className = {styles.imageUpdates} />
+};
+
+export default RecipePhoto;
--- src/components/Description.js
+++ src/components/Description.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import styles from './Description.module.css'
-function RecipeAuthor() {
-    let authorLink;
-    let authorPhoto;
-    let authorName;
+import styles from './Description.module.css';
+import { Component } from 'react';
+
+let recipeTitle = "Cinnamon Rolls";
+let recipeShortDesc = "Delicious cinnamon rolls with frosting"
+
+const RecipeAuthor = ()  =>{
+    let authorLink = "https://www.kingarthurbaking.com/author/charlotte-rutledge";
+    let authorPhoto = "https://www.kingarthurbaking.com/sites/default/files/styles/scaled_very_small_250w/public/2022-06/Charlotte-Rutledge-.jpg?itok=UJJypdjY";
+    let authorName = "Charlotte Rutledge";
 
     return (
-        <div className = {StyleSheet.recipeAuthorBlock}>
-            <img src={authorphoto} alt=""
-            className={StyleSheet.imageUpdates} />
+        <div className = {styles.recipeAuthorBlock}>
+            <img src={authorPhoto} 
+            alt="Charlotte Rutledge"
+            className={styles.imageUpdates} />
             <div>
                 <h3>{authorName}</h3>
-                <a href={authorLink}>Blog name </a>
+                <a href={authorLink}> Pastries for the Win </a>
                 </div>
         </div>
         )};
+
+        class RecipeDescription extends Component {
+            render () {
+                return(
+                    <div>
+                        <div>
+                            <h1>{recipeTitle}</h1>
+                            <p>{recipeShortDesc}</p>
+                        </div>
+                        <RecipeAuthor />
+                    </div>
+                )
+            }
+
+        }
+        export default RecipeDescription;
